2ae7a947d14244065d8f59ed91e5bf0e65a2463a
Zakk Heile <zwh4@duke.edu>
2025-02-24 19:26:25 -0500

    Defining amount of memory (trivial)

--- src/ddb/executor/join/hasheqj.py
+++ src/ddb/executor/join/hasheqj.py
@@ -154,5 +154,7 @@ class HashEqJoinPop(JoinPop['HashEqJoinPop.CompiledProps']):
     def execute(self) -> Generator[tuple, None, None]:
         # THIS IS WHERE YOUR IMPLEMENTATION SHOULD GO
         # but feel free to define other helper methods in this class as you see fit
+        M = self.num_memory_blocks
+        N = M-1
         yield from ()
         return
